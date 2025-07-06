3c9c6af7517e4d8971f633cbf629c65cff82208e
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-07-06 16:40:56 +0530

    change in the files

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://classmate.onrender.com",
+    "https://classmate-frontend.onrender.com",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
